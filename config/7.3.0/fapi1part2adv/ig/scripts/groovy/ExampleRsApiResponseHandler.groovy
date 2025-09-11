a2e38e7923aa0d606fe9a12a8b47508e1f16acf2
Wayne Morrison <wayne.morrison@pingidentity.com>
2025-09-11 11:14:29 +0100

    do-not-merge: some random change

--- config/7.3.0/fapi1part2adv/ig/scripts/groovy/ExampleRsApiResponseHandler.groovy
+++ config/7.3.0/fapi1part2adv/ig/scripts/groovy/ExampleRsApiResponseHandler.groovy
@@ -5,5 +5,5 @@ logger.debug(SCRIPT_NAME + "Creating example API response...")
 Response response = new Response(Status.OK)
 var sub = contexts.oauth2.accessToken.info["sub"]
 response.entity.json = json(object(field("user", sub)))
-
+// some temp change
 return response
